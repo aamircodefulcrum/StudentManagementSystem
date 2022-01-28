14e073b979f40dbc16c3a4c45603e82974010173
aamir <amir.zaman@codefulcrum.com>
2022-01-28 21:55:21 +0500

    Extra spaces were added as per PEP8 rules

--- UserStudentApp/serializers.py
+++ UserStudentApp/serializers.py
@@ -1,30 +1,32 @@
 from rest_framework import serializers
 from .models import Student, CustomUser, Course, Image
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    student = serializers.HyperlinkedRelatedField(many = True, view_name = 'student-detail', read_only = True)
+    students = serializers.HyperlinkedRelatedField(many = True, view_name = 'student-detail', read_only = True)
 
     class Meta:
         model  = CustomUser
-        fields = ['id', 'url', 'username', 'email', 'contact_num', 'age', 'student', 'profile_pic', 'images']
+        fields = ['id', 'url', 'username', 'email', 'contact_num', 'age', 'students', 'profile_pic', 'images']
+
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source = 'owner.name')
-    # url   = serializers.HyperlinkedIdentityField(view_name = 'student-detail')
 
     class Meta:
         model  = Student
         fields = ['id', 'url', 'owner', 'name', 'age', 'city', 'is_activated', 'courses']
+
 
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
     students = serializers.HyperlinkedIdentityField(many = True, view_name = 'student-detail', read_only = True)
 
     class Meta:
         model  = Course
-        fields = ['id', 'url', 'course_name', 'students']
+        fields = ['id', 'url', 'name', 'students']
+
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
-    # images = serializers.HyperlinkedIdentityField(many= True, view_name= 'image-details') 
     class Meta:
         model  = Image
         fields = ['id', 'other_pics', 'images'] 
